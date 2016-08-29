f8f53bc778a869560447897dd15a2316be6d73cb
sverweij <sverweij@users.noreply.github.com>
2016-08-29 22:43:24 +0200

    [test] adds tests for ast2stategenny

--- test/render/ast2stategenny.spec.js
+++ test/render/ast2stategenny.spec.js
@@ -2,12 +2,18 @@ const expect  = require('chai').expect;
 const convert = require('../../src/render/ast2stategenny');
 const parser  = require('../../src/parse/stategenny-parser');
 
-describe("#render - ast2stategenny", () => {
-    describe("1:1 tests", () => {
-        it("on comments-wherever.json", () => {
-            let lAST = require("../parse/fixtures/comments-wherever.json");
-            let lProgram = convert.render(lAST);
-            expect(parser.parse(lProgram)).to.deep.equal(lAST);
+const programASTPairs =
+        require("../parse/00-no-transitions.json")
+        .concat(require("../parse/01-transitions-only.json"));
+
+describe('#parse(convert) - happy day ASTs - ', () => {
+    programASTPairs.forEach(pPair => {
+        it(pPair.title, () => {
+            expect(
+                parser.parse(convert.render(pPair.ast))
+            ).to.deep.equal(
+                pPair.ast
+            );
         });
     });
 });
