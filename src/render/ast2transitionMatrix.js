d2967fb2ddbe17e6af3d22dc206c0b659b54cc4c
sverweij <sverweij@users.noreply.github.com>
2016-09-23 21:35:52 +0200

    :bug: adds a require parameter so ast2transitionMatrix works under requirejs

--- src/render/ast2transitionMatrix.js
+++ src/render/ast2transitionMatrix.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') {
     var define = require('amdefine')(module);
 }
 
-define(function() {
+define(function(require) {
     "use strict";
     var _ = require("./utl");
 
