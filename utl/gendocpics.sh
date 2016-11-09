6d27b9baed0be08097d52d2df1897d42f70a19a3
sverweij <sverweij@users.noreply.github.com>
2016-11-09 23:07:07 +0100

    :book: adds documentation on using pseudo states 'choice', 'fork' and 'join' to the README

--- utl/gendocpics.sh
+++ utl/gendocpics.sh
@@ -6,6 +6,8 @@ bin/smcat -T dot -o - $DIR/01labels.smcat | circo -Tpng -o$DIR/01labels.png
 bin/smcat -T dot -o - $DIR/01labels_better.smcat | dot -Tpng -o$DIR/01labels_better.png
 bin/smcat -T dot -o - $DIR/02notes.smcat | dot -Tpng -o$DIR/02notes.png
 bin/smcat -T dot -o - $DIR/03initial_and_final.smcat | circo -Tpng -o$DIR/03initial_and_final.png
+bin/smcat -T dot -o - $DIR/03achoice.smcat | circo -Tpng -o$DIR/03achoice.png
+bin/smcat -T dot -o - $DIR/03bforkjoin.smcat | dot -Tpng -o$DIR/03bforkjoin.png
 bin/smcat -T dot -o - $DIR/04explicit_state_declarations.smcat | circo -Tpng -o$DIR/04explicit_state_declarations.png
 bin/smcat -T dot -o - $DIR/05tape_player.smcat | dot -Tpng -o$DIR/05tape_player.png
 
