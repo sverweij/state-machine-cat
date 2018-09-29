8ec54a8a78ba2cd8da0b73f1da6c5d95dc5c30f0
sverweij <sverweij@users.noreply.github.com>
2018-09-29 16:22:04 +0200

    test(fixtures): consolidate e2e/ integration test fixture regenration scripts

--- utl/regenerate_render_fixtures.sh
+++ utl/regenerate_render_fixtures.sh
@@ -6,3 +6,21 @@ find -X test/render/fixtures/*.smcat -exec bin/smcat -T json  {} ";" & \
 find -X test/render/fixtures/*.smcat -exec bin/smcat -T scjson  {} ";" & \
 find -X test/render/fixtures/*.smcat -exec bin/smcat -T scxml  {} ";" & \
 find -X test/render/fixtures/*.smcat -exec bin/smcat -T svg  {} ";"
+bin/smcat -T json test/parse/fixtures/composite.smcat
+bin/smcat -T dot test/parse/fixtures/composite.smcat
+bin/smcat -T html test/parse/fixtures/composite.smcat
+bin/smcat -T dot --direction left-right test/parse/fixtures/composite.smcat -o test/parse/fixtures/composite-left-right.dot
+bin/smcat -T json test/parse/fixtures/composite_no_root_transitions.smcat
+bin/smcat -T dot test/parse/fixtures/composite_no_root_transitions.smcat
+bin/smcat -T html test/parse/fixtures/composite_no_root_transitions.smcat
+bin/smcat -T json test/parse/fixtures/kitchensink.smcat
+bin/smcat -T dot test/parse/fixtures/kitchensink.smcat
+bin/smcat -T html test/parse/fixtures/kitchensink.smcat
+bin/smcat -T json test/parse/fixtures/minimal.smcat
+bin/smcat -T dot test/parse/fixtures/minimal.smcat
+bin/smcat -T html test/parse/fixtures/minimal.smcat
+bin/smcat -T dot test/parse/fixtures/pseudostates.smcat
+bin/smcat -T json test/parse/fixtures/pseudostates.smcat
+bin/smcat -T html test/parse/fixtures/states-with-a-label.smcat
+bin/smcat -T dot test/parse/fixtures/states-with-a-label.smcat
+bin/smcat -T json test/parse/fixtures/states-with-a-label.smcat
