9434a0cc10bd8cfc04ba0494732f0e8ab8de9c50
sverweij <sverweij@users.noreply.github.com>
2018-12-20 21:17:17 +0100

    build(npm): :arrow_up: ajv, webpack, eslint, dependency-cruiser
    ... and build a dependency-cruiser graph that's slightly more interactive

--- config/dependency-cruiser-graph.js
+++ config/dependency-cruiser-graph.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: './dependency-cruiser',
     options: {
-        exclude: 'node_modules'
+        exclude: 'node_modules',
+        prefix: 'https://github.com/sverweij/state-machine-cat/blob/develop'
     }
 };
