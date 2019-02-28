6bc5daa3b34c2e738360deae5feb4638cd43ad1f
sverweij <sverweij@users.noreply.github.com>
2019-02-28 22:06:16 +0100

    doc(graph): regenerate the dependency graph with a new header

--- config/dependency-cruiser-graph.js
+++ config/dependency-cruiser-graph.js
@@ -2,6 +2,6 @@ module.exports = {
     extends: './dependency-cruiser',
     options: {
         exclude: 'node_modules',
-        prefix: 'https://github.com/sverweij/state-machine-cat/blob/develop'
+        prefix: 'https://github.com/sverweij/state-machine-cat/blob/develop/'
     }
 };
