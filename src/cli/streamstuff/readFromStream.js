5306723268a78d9340c98f0ba643ce17f5b606f5
sverweij <sverweij@users.noreply.github.com>
2019-04-07 12:44:25 +0200

    refactor(cli): use chaining for stream reading

--- src/cli/streamstuff/readFromStream.js
+++ src/cli/streamstuff/readFromStream.js
@@ -1,22 +1,24 @@
 module.exports = function (pInStream) {
     return new Promise((pResolve, pReject) => {
         let lInput = "";
-        pInStream.resume();
-        pInStream.setEncoding("utf8");
-        pInStream.on("data", (pChunk) => {
-            lInput += pChunk;
-        });
-        pInStream.on("end", () => {
-            try {
-                pInStream.pause();
-                pResolve(lInput);
-            }
-            catch (e) {
+
+        pInStream
+            .resume()
+            .setEncoding("utf8")
+            .on("data", (pChunk) => {
+                lInput += pChunk;
+            })
+            .on("end", () => {
+                try {
+                    pInStream.pause();
+                    pResolve(lInput);
+                }
+                catch (e) {
+                    pReject(e);
+                }
+            })
+            .on("error", (e) => {
                 pReject(e);
-            }
-        });
-        pInStream.on("error", (e) => {
-            pReject(e);
-        });
+            });
     });
 };
