c9d8989a7a35950f1ef8d236a1ada954dffddf50
Sander Verweij <sverweij@users.noreply.github.com>
2019-07-30 21:04:55 +0200

    bugfix(parse-scxml): improve initial recognition (#64)

--- src/parse/scxml/normalizeMachine.js
+++ src/parse/scxml/normalizeMachine.js
@@ -4,7 +4,8 @@ const arrayify = require('./utl').arrayify;
 
 function normalizeInitialFromObject(pMachine) {
     const lRetval = {
-        id: "initial"
+        // ensure the 'initial' state has a unique name
+        id: pMachine.id ? `${pMachine.id}.initial` : "initial"
     };
     if (pMachine.initial.transition) {
         Object.assign(
@@ -37,9 +38,14 @@ function normalizeInitial(pMachine) {
     let lInitialObject = {};
 
     if (pMachine.initial) {
-        if (pMachine.initial.id) {
+        // => it's an xml node. This detection isn't fool proof...;
+        // if it's a node but it doesn't have a transtion (which
+        // looks like an odd corner case) we won't recognize the
+        // initial
+        // the initial.id shouldn't occur (not allowed in scxml
+        // land), but smcat scxml renderer generates it nonetheless
+        if (pMachine.initial.transition || pMachine.initial.id) {
             lInitialObject = normalizeInitialFromObject(pMachine);
-
         } else {
             lInitialObject = normalizeInitialFromString(pMachine);
         }
