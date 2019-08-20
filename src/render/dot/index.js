3eda5a330387c4e100f5c8bc73c2e78e7133b6d7
sverweij <sverweij@users.noreply.github.com>
2019-08-20 22:13:09 +0200

    refactor(dot): mcsplit & extracterington

--- src/render/dot/index.js
+++ src/render/dot/index.js
@@ -1,16 +1,17 @@
 const Handlebars = require("handlebars/dist/handlebars.runtime");
 const _cloneDeep = require("lodash.clonedeep");
-const _get = require("lodash.get");
 const options = require("../../options");
 const StateMachineModel = require("../../stateMachineModel");
-const Counter = require("./counter");
 const attributebuilder = require("./attributebuilder");
-
-/* eslint import/no-unassigned-import: 0 */
-require("./dot.template");
-require("./dot.states.template");
+const stateTransformers = require("./state-transformers");
+const transitionTransformers = require("./transition-transformers");
+const Counter = require("./counter");
 
 let gCounter = {};
+
+/* eslint import/no-unassigned-import: 0, import/max-dependencies: 0 */
+require("./dot.template");
+require("./dot.states.template");
 
 Handlebars.registerPartial(
   "dot.states.template.hbs",
@@ -21,118 +22,6 @@ Handlebars.registerHelper("stateSection", pStateMachine =>
   Handlebars.templates["dot.states.template.hbs"](splitStates(pStateMachine))
 );
 
-function isType(pString) {
-  return function(pState) {
-    return pState.type === pString;
-  };
-}
-function isOneOfTypes(pStringArray) {
-  return function(pState) {
-    return pStringArray.indexOf(pState.type) >= 0;
-  };
-}
-
-function setLabel(pState) {
-  pState.label = pState.label || pState.name;
-  return pState;
-}
-
-function nameNote(pState) {
-  if (pState.hasOwnProperty("note")) {
-    pState.noteName = `note_${pState.name}`;
-  }
-  return pState;
-}
-
-function flattenNote(pState) {
-  if (pState.hasOwnProperty("note")) {
-    pState.noteFlattened = pState.note.join("");
-  }
-  return pState;
-}
-
-function escapeString(pString) {
-  return pString
-    .replace(/\\/g, "\\\\")
-    .replace(/\n\s*/g, "\\l")
-    .replace(/"/g, '\\"')
-    .concat("\\l");
-}
-
-function escapeLabelString(pString) {
-  return pString
-    .replace(/\\/g, "\\\\")
-    .replace(/\n\s*/g, "   \\l")
-    .replace(/"/g, '\\"')
-    .concat("   \\l");
-}
-
-function escapeStateStrings(pState) {
-  if (pState.note) {
-    pState.note = pState.note.map(escapeString);
-  }
-  return pState;
-}
-
-function escapeTransitionStrings(pTransition) {
-  if (pTransition.note) {
-    pTransition.note = pTransition.note.map(escapeString);
-  }
-  if (pTransition.label) {
-    pTransition.label = escapeLabelString(pTransition.label);
-  }
-  return pTransition;
-}
-
-function formatActionType(pString) {
-  return pString === "activity" ? "" : `${pString}/ `;
-}
-
-function flattenActions(pState) {
-  const lRetval = Object.assign({}, pState);
-
-  if (pState.actions) {
-    lRetval.actions = pState.actions.map(
-      pAction => `${formatActionType(pAction.type)}${pAction.body}`
-    );
-  }
-
-  return lRetval;
-}
-
-function isVertical(pDirection) {
-  const lDirection = pDirection || "top-down";
-  return lDirection === "top-down" || lDirection === "bottom-top";
-}
-
-function tipForkJoinStates(pDirection) {
-  return function(pState) {
-    if (isOneOfTypes(["fork", "join", "forkjoin"])(pState)) {
-      return Object.assign(
-        {
-          sizingExtras: isVertical(pDirection) ? "height=0.1" : "width=0.1"
-        },
-        pState
-      );
-    }
-    return pState;
-  };
-}
-
-function flagParallelChildren(pState) {
-  if (pState.type === "parallel") {
-    if (pState.statemachine && pState.statemachine.states) {
-      pState.statemachine.states = pState.statemachine.states
-        .filter(isType("regular"))
-        .map(pChildState =>
-          Object.assign({}, pChildState, { parentIsParallel: true })
-        );
-    }
-  }
-
-  return pState;
-}
-
 function addSelfTransitionsFlag(pStateMachineModel) {
   return pState => {
     if (
@@ -140,25 +29,6 @@ function addSelfTransitionsFlag(pStateMachineModel) {
       pStateMachineModel.stateHasSelfTransitions(pState.name)
     ) {
       pState.hasSelfTransitions = true;
-    }
-    return pState;
-  };
-}
-
-function recolor(pNodeAttrs) {
-  return pState => {
-    const lNodeColor = _get(
-      (pNodeAttrs || []).find(pAttr => pAttr.name === "color"),
-      "value"
-    );
-    if (
-      lNodeColor &&
-      !pState.color &&
-      ["initial", "fork", "join", "junction", "forkjoin", "final"].includes(
-        pState.type
-      )
-    ) {
-      pState.color = lNodeColor;
     }
     return pState;
   };
@@ -177,31 +47,38 @@ function transformStates(pStates, pDirection, pNodeAttrs, pStateMachineModel) {
     });
 
   return pStates
-    .map(setLabel)
-    .map(nameNote)
-    .map(escapeStateStrings)
-    .map(flattenNote)
-    .map(flattenActions)
-    .map(flagParallelChildren)
-    .map(tipForkJoinStates(pDirection))
-    .map(recolor(pNodeAttrs))
+    .map(stateTransformers.setLabel)
+    .map(stateTransformers.nameNote)
+    .map(stateTransformers.escapeStateStrings)
+    .map(stateTransformers.flattenNote)
+    .map(stateTransformers.flattenActions)
+    .map(stateTransformers.flagParallelChildren)
+    .map(stateTransformers.tipForkJoinStates(pDirection))
+    .map(stateTransformers.recolor(pNodeAttrs))
     .map(addSelfTransitionsFlag(pStateMachineModel));
 }
 
 function splitStates(pAST) {
-  pAST.initialStates = pAST.states.filter(isType("initial"));
+  pAST.initialStates = pAST.states.filter(stateTransformers.isType("initial"));
   pAST.regularStates = pAST.states.filter(
-    pState => isType("regular")(pState) && !pState.statemachine
+    pState =>
+      stateTransformers.isType("regular")(pState) && !pState.statemachine
   );
-  pAST.historyStates = pAST.states.filter(isType("history"));
-  pAST.deepHistoryStates = pAST.states.filter(isType("deephistory"));
-  pAST.choiceStates = pAST.states.filter(isType("choice"));
+  pAST.historyStates = pAST.states.filter(stateTransformers.isType("history"));
+  pAST.deepHistoryStates = pAST.states.filter(
+    stateTransformers.isType("deephistory")
+  );
+  pAST.choiceStates = pAST.states.filter(stateTransformers.isType("choice"));
   pAST.forkjoinStates = pAST.states.filter(
-    isOneOfTypes(["fork", "join", "forkjoin"])
+    stateTransformers.isOneOfTypes(["fork", "join", "forkjoin"])
   );
-  pAST.junctionStates = pAST.states.filter(isType("junction"));
-  pAST.terminateStates = pAST.states.filter(isType("terminate"));
-  pAST.finalStates = pAST.states.filter(isType("final"));
+  pAST.junctionStates = pAST.states.filter(
+    stateTransformers.isType("junction")
+  );
+  pAST.terminateStates = pAST.states.filter(
+    stateTransformers.isType("terminate")
+  );
+  pAST.finalStates = pAST.states.filter(stateTransformers.isType("final"));
   pAST.compositeStates = pAST.states.filter(pState => pState.statemachine);
 
   return pAST;
@@ -233,36 +110,6 @@ function addCompositeSelfFlag(pStateMachineModel) {
   };
 }
 
-function addPorts(pDirection) {
-  return pTransition => {
-    let lAdditionalAttributes = {};
-    if (pTransition.isCompositeSelf) {
-      if (isVertical(pDirection)) {
-        lAdditionalAttributes = {
-          tailportflags: `tailport="e" headport="e"`,
-          headportflags: `tailport="w"`
-        };
-      } else {
-        lAdditionalAttributes = {
-          tailportflags: `tailport="s" headport="s"`,
-          headportflags: `tailport="n"`
-        };
-      }
-    }
-    return Object.assign({}, pTransition, lAdditionalAttributes);
-  };
-}
-
-function transformTransitions(pStateMachineModel, pDirection) {
-  return pStateMachineModel.flattenedTransitions
-    .map(nameTransition)
-    .map(escapeTransitionStrings)
-    .map(flattenNote)
-    .map(addEndTypes(pStateMachineModel))
-    .map(addCompositeSelfFlag(pStateMachineModel))
-    .map(addPorts(pDirection));
-}
-
 function nameTransition(pTrans) {
   pTrans.name = "tr_${from}_${to}_${counter}"
     .replace(/\${from}/g, pTrans.from)
@@ -274,6 +121,16 @@ function nameTransition(pTrans) {
   }
 
   return pTrans;
+}
+
+function transformTransitions(pStateMachineModel, pDirection) {
+  return pStateMachineModel.flattenedTransitions
+    .map(nameTransition)
+    .map(transitionTransformers.escapeTransitionStrings)
+    .map(stateTransformers.flattenNote)
+    .map(addEndTypes(pStateMachineModel))
+    .map(addCompositeSelfFlag(pStateMachineModel))
+    .map(transitionTransformers.addPorts(pDirection));
 }
 
 module.exports = (pAST, pOptions) => {
---
+++ src/render/dot/state-transformers.js
@@ -0,0 +1,119 @@
+const _get = require("lodash.get");
+const utl = require("./utl");
+
+function isType(pString) {
+  return function(pState) {
+    return pState.type === pString;
+  };
+}
+function isOneOfTypes(pStringArray) {
+  return function(pState) {
+    return pStringArray.indexOf(pState.type) >= 0;
+  };
+}
+
+function setLabel(pState) {
+  pState.label = pState.label || pState.name;
+  return pState;
+}
+
+function nameNote(pState) {
+  if (pState.hasOwnProperty("note")) {
+    pState.noteName = `note_${pState.name}`;
+  }
+  return pState;
+}
+
+function formatActionType(pString) {
+  return pString === "activity" ? "" : `${pString}/ `;
+}
+
+function flattenActions(pState) {
+  const lRetval = Object.assign({}, pState);
+
+  if (pState.actions) {
+    lRetval.actions = pState.actions.map(
+      pAction => `${formatActionType(pAction.type)}${pAction.body}`
+    );
+  }
+
+  return lRetval;
+}
+function flattenNote(pState) {
+  if (pState.hasOwnProperty("note")) {
+    pState.noteFlattened = pState.note.join("");
+  }
+  return pState;
+}
+
+function recolor(pNodeAttrs) {
+  return pState => {
+    const lNodeColor = _get(
+      (pNodeAttrs || []).find(pAttr => pAttr.name === "color"),
+      "value"
+    );
+    if (
+      lNodeColor &&
+      !pState.color &&
+      isOneOfTypes([
+        "initial",
+        "fork",
+        "join",
+        "junction",
+        "forkjoin",
+        "final"
+      ])(pState)
+    ) {
+      pState.color = lNodeColor;
+    }
+    return pState;
+  };
+}
+
+function escapeStateStrings(pState) {
+  if (pState.note) {
+    pState.note = pState.note.map(utl.escapeString);
+  }
+  return pState;
+}
+
+function tipForkJoinStates(pDirection) {
+  return function(pState) {
+    if (isOneOfTypes(["fork", "join", "forkjoin"])(pState)) {
+      return Object.assign(
+        {
+          sizingExtras: utl.isVertical(pDirection) ? "height=0.1" : "width=0.1"
+        },
+        pState
+      );
+    }
+    return pState;
+  };
+}
+
+function flagParallelChildren(pState) {
+  if (pState.type === "parallel") {
+    if (pState.statemachine && pState.statemachine.states) {
+      pState.statemachine.states = pState.statemachine.states
+        .filter(isType("regular"))
+        .map(pChildState =>
+          Object.assign({}, pChildState, { parentIsParallel: true })
+        );
+    }
+  }
+
+  return pState;
+}
+
+module.exports = {
+  isType,
+  isOneOfTypes,
+  setLabel,
+  nameNote,
+  flattenActions,
+  flattenNote,
+  recolor,
+  escapeStateStrings,
+  tipForkJoinStates,
+  flagParallelChildren
+};
