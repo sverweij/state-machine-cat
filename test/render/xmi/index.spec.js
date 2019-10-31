5993f4538c3a5de5360aa0360d47a9c6f34bcaef
sverweij <sverweij@users.noreply.github.com>
2019-10-31 22:18:07 +0100

    only lhs

--- test/render/xmi/index.spec.js
+++ test/render/xmi/index.spec.js
@@ -15,7 +15,7 @@ describe("#ast2xmi - integration - ", () => {
       const lResult = convert(
         JSON.parse(fs.readFileSync(pInputFixture, "utf8"))
       );
-      expect(lResult.replace(/\r/g, "")).to.deep.equal(
+      expect(lResult).to.deep.equal(
         fs
           .readFileSync(pInputFixture.replace(/\.json$/g, ".xmi"), "utf8")
           .replace(/\r/g, "")
