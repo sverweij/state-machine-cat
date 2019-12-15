d269141bd0ed23e5e57c9dd12d39072bfa95fd3e
sverweij <sverweij@users.noreply.github.com>
2019-12-15 14:57:00 +0100

    doc(*): expand json schema and type documentation

--- types/state-machine-cat.d.ts
+++ types/state-machine-cat.d.ts
@@ -23,33 +23,42 @@ export interface IActionType {
 
 export interface IState {
   /**
-   * the name and identifier of the state
+   * The name and identifier of the state. Unique within the root state machine.
    */
   name: string;
   /**
-   * what kind of state (or pseudo state) this state is. E.g. 'regular'
+   * What kind of state (or pseudo state) this state is. E.g. 'regular'
    * for normal states or 'initial', 'final', 'choice' etc for pseudo states
    */
   type: StateType;
   /**
-   * the display label of the state. If it's not present, most renderers
+   * The display label of the state. If it's not present, most renderers
    * will use the states' name in stead
    */
   label?: string;
+  /**
+   * A series of actions and their types. The type describe when the action
+   * takes place (on entry, exit, or otherwise ('activity'))
+   */
   actions?: IActionType[];
   /**
-   * state machine nested within the stated
+   * State machine nested within the state.
    */
   statemachine?: IStateMachine;
+  /**
+   * If true the state is considered to be active and rendered as such.
+   */
   active?: boolean;
   /**
-   * some renderers will use the color attribute to color the state
-   * in their output
+   * Color to use for rendering the state. Accepts all css color names
+   * (\"blue\") and hex notation - with (\"#0000FF77\") or without
+   * (\"#0000FF\") transparency.
    */
   color?: string;
   /**
-   * some renderers will use the note attribute to render a note
-   * (i.e. as a post-it) attached to the stated
+   * Comments related to this state. Some renderers will use the note
+   * attribute to render a note (i.e. as a post-it) attached to the
+   * state.
    */
   note?: string[];
   /**
@@ -70,25 +79,44 @@ export interface IState {
 
 export interface ITransition {
   /**
-   * reference to the name of the IState the transition is from
+   * The name of the IState the transition is from
    */
   from: string;
   /**
-   * reference to the name of the IState the transition is from
+   * The name of the IState the transition is to
    */
   to: string;
   /**
-   * display label of he transition
+   * A display label to represent this transition. Parsers can parse this
+   * label into events conditions and actions.
    */
   label?: string;
+  /**
+   * Event triggering the transition
+   */
   event?: string;
+  /**
+   * Condition for the transition to occur.
+   */
   cond?: string;
+  /**
+   * Action to execute when the transition occurs.
+   */
   action?: string;
+  /**
+   * Comments related to this transition. Some renderers will use the note
+   * attribute to render a note attached to the transition.
+   */
   note?: string[];
+  /**
+   * Color to use for rendering the transition. Accepts all css color
+   * names (\"blue\") and hex notation - with (\"#0000FF77\") or without
+   * (\"#0000FF\") transparency.
+   */
   color?: string;
   /**
-   * Whether the transition is "external" (the default) or "internal"
-   * See https://www.w3.org/TR/scxml/#transition for details
+   * Whether the transition is "external" (the default) or "internal".
+   * See https://www.w3.org/TR/scxml/#transition for details.
    */
   type?: TransitionType;
 }
@@ -128,12 +156,12 @@ export interface IAllowedValues {
 }
 
 /**
- * An object with for each of the options you can pass to
- * the render function
- * - the default value
- * - the possible values in an array of objects, each of which
- *   has the properties:
- *   - name: the value
+ * @returns An object with for each of the options you can pass to
+ *          the render function:
+ *          - the default value
+ *          - the possible values in an array of objects, each of which
+ *            has the properties:
+ *          - name: the value
  *
  */
 export function getAllowedValues(): IAllowedValues;
@@ -165,26 +193,55 @@ export type dotAttrsType = Array<{
 }>;
 
 export interface IRenderOptions {
+  /**
+   * How to interpret the input (defaults to 'smcat')
+   */
   inputType?: InputType;
+  /**
+   * What renderer to use (defaults to 'svg')
+   */
   outputType?: OutputType;
+  /**
+   * For the 'dot' renderer: what engine to use (defaults to 'dot')
+   */
   engine?: EngineType;
+  /**
+   * For the 'dot' renderer: in what direction to plot the states
+   * (defaults to 'top-down')
+   */
   direction?: DirectionType;
+  /**
+   * For the 'dot' renderer: Graph attributes to the engine
+   */
   dotGraphAttrs?: dotAttrsType;
+  /**
+   * For the 'dot' renderer: Node attributes to the engine
+   */
   dotNodeAttrs?: dotAttrsType;
+  /**
+   * For the 'dot' renderer: Edge attributes to the engine
+   */
   dotEdgeAttrs?: dotAttrsType;
+  /**
+   * If true state machine cat will replace 'sugar' pseudo states
+   * (choice, forks and junctions) with their equivalent meaning
+   * (defaults to false).
+   *
+   * For details: https://github.com/sverweij/state-machine-cat/blob/master/docs/desugar.md
+   */
   desugar?: boolean;
 }
 
 /**
  * Translates the input script to an outputscript.
  *
- * @param  {string} pScript     The script to translate
- * @param  {object} pOptions    options influencing parsing & rendering.
- *                              See below for the complete list.
- * @return {string}             the string with the rendered content if
- *                              no error was found
- * @throws {Error}              if an error occurred and no callback
- *                              function was passed: the error
+ * @param pScript     The script to translate
+ * @param pOptions    options influencing parsing & rendering.
+ *                    See below for the complete list.
+ * @return            the string with the rendered content if
+ *                    no error was found
+ * @throws            If an error occurred and no callback
+ *                    function was passed: the error
  *
  * Options: see https://github.com/sverweij/state-machine-cat/docs/api.md
  *
