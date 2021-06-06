22f7b13ede4834eaefc26b79c05030cd1f28821f
sverweij <sverweij@users.noreply.github.com>
2021-06-06 11:12:44 +0200

    feature(inpage): lazy load graphs with IntersectionObserver

--- docs/state-machine-cat-inpage.js
+++ docs/state-machine-cat-inpage.js
@@ -18,13 +18,15 @@ function getResponseText(pResponse) {
   return pResponse.text();
 }
 
-function getScriptSrc(pScript) {
+function getScriptSource(pScript) {
   const lSourceURL = pScript.getAttribute("src");
   if (lSourceURL) {
     return fetch(lSourceURL).then(getResponseStatus).then(getResponseText);
   }
+  // when there's no "src" attribute, return the textContent
+  // (in a Promise to have a consistent interface)
   return new Promise((pResolve, pReject) => {
-    if (pScript.textContent) {
+    if (pScript.textContent && pScript.textContent.length > 0) {
       pResolve(pScript.textContent);
     } else {
       pReject(new Error("ERROR: this element doesn't contain any text"));
@@ -62,43 +64,70 @@ function renderInElement(pSource, pOptions) {
     }
   }
 }
-
-function renderAllScriptElements() {
-  [...document.scripts]
-    .filter(
-      (pScript) =>
-        !!MIME2LANG[pScript.type] && !pScript.hasAttribute("data-renderedby")
-    )
-    .forEach((pScript) => {
-      getScriptSrc(pScript)
-        .then((pSrc) => {
-          pScript.after(
-            renderInElement(pSrc, {
-              inputType: MIME2LANG[pScript.type],
-              outputType: pScript.getAttribute("data-output-type") || "svg",
-              direction: pScript.getAttribute("data-direction") || "top-down",
-              engine: pScript.getAttribute("data-engine") || "dot",
-              desugar: pScript.getAttribute("data-desugar") || false,
-              dotGraphAttrs: [{ name: "bgcolor", value: "transparent" }],
-            })
-          );
-          pScript.setAttribute("data-renderedby", "state-machine-cat");
-        })
-        .catch((pErr) => {
-          pScript.after(
-            renderCodeError(
-              `Could not render ${
-                pScript.src
-                  ? `"${pScript.src}"`
-                  : (pScript.textContent && "provided text content") ||
-                    "(no text content)"
-              }${pErr ? `: ${pErr}` : ""}`
-            )
-          );
-        });
-    });
+function renderScript(pScriptElement) {
+  if (!pScriptElement.dataset.renderedby) {
+    pScriptElement.dataset.renderedby = "state-machine-cat";
+    getScriptSource(pScriptElement)
+      .then((pSrc) => {
+        pScriptElement.after(
+          renderInElement(pSrc, {
+            inputType: MIME2LANG[pScriptElement.type],
+            outputType:
+              pScriptElement.getAttribute("data-output-type") || "svg",
+            direction: pScriptElement.dataset.direction || "top-down",
+            engine: pScriptElement.dataset.engine || "dot",
+            desugar: pScriptElement.dataset.desugar || false,
+            dotGraphAttrs: [{ name: "bgcolor", value: "transparent" }],
+          })
+        );
+      })
+      .catch((pErr) => {
+        pScriptElement.after(
+          renderCodeError(
+            `Could not render ${
+              pScriptElement.src
+                ? `"${pScriptElement.src}"`
+                : (pScriptElement.textContent && "provided text content") ||
+                  "(no text content)"
+            }${pErr ? `: ${pErr}` : ""}`
+          )
+        );
+      });
+  }
 }
 
-renderAllScriptElements();
+function observerCallback(pEntries, _pObserver) {
+  pEntries.forEach((pEntry) => {
+    if (pEntry.isIntersecting) {
+      const lScriptElement = pEntry.target.previousElementSibling;
+      if (lScriptElement.tagName === "SCRIPT") {
+        renderScript(lScriptElement);
+      }
+    }
+  });
+}
+
+let OBSERVER = new IntersectionObserver(observerCallback, { threshold: 1 });
+
+const SCRIPT_ELEMENTS = [...document.scripts].filter(
+  (pScript) => !!MIME2LANG[pScript.type]
+);
+
+SCRIPT_ELEMENTS.forEach((pScriptElement) => {
+  // scripts are not visible, hence observing them for visibility
+  // is doing nothing. Workaround: insert a marker element right
+  // after it that _is_ visible, and observe that.
+  const lScriptMarker = document.createElement("smcat-marker");
+  pScriptElement.after(lScriptMarker);
+
+  OBSERVER.observe(lScriptMarker);
+});
+
+// Observer trickery, of course, is nice, but when you print a page
+// you want all of the graphs to show up anyway. This ensures that
+// that indeed happens at the right time:
+window.addEventListener("beforeprint", (_pEvent) => {
+  SCRIPT_ELEMENTS.forEach(renderScript);
+});
 
 /* eslint security/detect-object-injection: 0 */
