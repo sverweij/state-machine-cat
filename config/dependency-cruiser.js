3c6727b82f71796999f6b3252f469d5bf223f177
Sander Verweij <sverweij@users.noreply.github.com>
2022-05-29 23:19:02 +0200

    chore: migrates part of the run scripts to wireit (#160)
    - all linting scripts
    - all dependency-cruiser validation & graph generating scripts
    - not yet: build scripts, release scripts

--- config/dependency-cruiser.js
+++ config/dependency-cruiser.js
@@ -222,12 +222,61 @@ module.exports = {
   options: {
     moduleSystems: ["cjs", "es6"],
     doNotFollow: "node_modules",
-    progress: { type: "performance-log" },
+    progress: { type: "none" },
     enhancedResolveOptions: {
       exportsFields: ["exports"],
       conditionNames: ["import"],
       extensions: [".js", ".mjs"],
     },
     exoticRequireStrings: ["requireJSON"],
+    prefix: "https://github.com/sverweij/state-machine-cat/blob/develop/",
+    reporterOptions: {
+      archi: {
+        collapsePattern: "^(bin|src/(cli|transform|[^/]+/[^/]+))",
+      },
+      dot: {
+        theme: {
+          graph: { splines: "ortho", ranksep: "0.5" },
+          modules: [
+            {
+              criteria: { source: "^src/cli" },
+              attributes: { fillcolor: "#ccffcc" },
+            },
+            {
+              criteria: { source: "^src/parse" },
+              attributes: { fillcolor: "#ffccff" },
+            },
+            {
+              criteria: { source: "^src/render" },
+              attributes: { fillcolor: "#ccccff" },
+            },
+            {
+              criteria: {
+                source: "(-parser|\\.template|\\.schema|version)\\.c?js$",
+              },
+              attributes: { style: "filled", color: "gray" },
+            },
+            {
+              criteria: { source: "\\.json$" },
+              attributes: { shape: "cylinder" },
+            },
+          ],
+          dependencies: [
+            {
+              criteria: { resolved: "^src/cli" },
+              attributes: { color: "#00770077" },
+            },
+            {
+              criteria: { resolved: "^src/parse" },
+              attributes: { color: "#ff00ff77" },
+            },
+            {
+              criteria: { resolved: "^src/render" },
+              attributes: { color: "#0000ff77" },
+            },
+          ],
+        },
+      },
+    },
   },
 };
