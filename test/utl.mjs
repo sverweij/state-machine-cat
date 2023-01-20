f9007329ed07d9837f2312ae304b1da7a716d9d0
sverweij <sverweij@users.noreply.github.com>
2023-01-20 18:46:33 +0100

    build(npm): update external dependencies
    Up'em just updated these outdated dependencies in package.json:
    
    @typescript-eslint/eslint-plugin  5.47.0    -> 5.48.2  devDependencies   (policy: latest)
    @typescript-eslint/parser         5.47.0    -> 5.48.2  devDependencies   (policy: latest)
    ajv                               8.11.2    -> 8.12.0  dependencies      (policy: latest)
    commander                         9.4.1     -> 10.0.0  dependencies      (policy: latest)
    dependency-cruiser                12.2.0    -> 12.5.1  devDependencies   (policy: latest)
    esbuild                           0.16.10   -> 0.17.3  devDependencies   (policy: latest)
    eslint                            8.30.0    -> 8.32.0  devDependencies   (policy: latest)
    eslint-config-prettier            8.5.0     -> 8.6.0   devDependencies   (policy: latest)
    eslint-plugin-import              2.26.0    -> 2.27.5  devDependencies   (policy: latest)
    eslint-plugin-security            1.5.0     -> 1.6.0   devDependencies   (policy: latest)
    husky                             8.0.2     -> 8.0.3   devDependencies   (policy: latest)
    prettier                          2.8.1     -> 2.8.3   devDependencies   (policy: latest)
    wireit                            0.9.2     -> 0.9.3   devDependencies   (policy: latest)
    
    Up'em found these packages were outdated, but did not update them because of policies:
    
    fast-xml-parser                   3.21.1  dependencies      (policy: wanted)
    viz.js                            1.8.2   dependencies      (policy: pin)

--- test/utl.mjs
+++ test/utl.mjs
@@ -2,6 +2,7 @@ import { readFileSync } from "node:fs";
 
 export function createRequireJSON(pBaseURL) {
   return function requireJSON(pString) {
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
     return JSON.parse(readFileSync(new URL(pString, pBaseURL), "utf8"));
   };
 }
