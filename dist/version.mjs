feef9aad19308c6ebca244f744778ca28c011f1a
sverweij <sverweij@users.noreply.github.com>
2026-05-10 21:08:15 +0200

    15.0.2

--- dist/version.mjs
+++ dist/version.mjs
@@ -1,1 +1,1 @@
-export const version = "15.0.1";
+export const version = "15.0.2";
---
+++ docs/interpreter/dot-W5CV4XZP.js
@@ -0,0 +1,2 @@
+import{a}from"./chunk-TSF5NPUK.js";import"./chunk-ZKNBFWW3.js";import"./chunk-WFFSAAHR.js";import"./chunk-FAD5XGJ4.js";import"./chunk-FW4363Y4.js";export{a as default};
+//# sourceMappingURL=dot-W5CV4XZP.js.map
